303473d47f6092ef4de77733d99ed5502d2b66de
Rodrigo Normando <r.normando@hotmail.com>
2020-08-11 12:54:44 +0200

    initial commit

---
+++ src/main/java/datasource/PostgressDataSource.java
@@ -0,0 +1,17 @@
+package datasource;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class PostgressDataSource  {
+    @Bean
+    @ConfigurationProperties("app.datasource")
+
+    public HikariDataSource hikariDataSOurce() {
+        return DataSourceBuilder.create().type(HikariDataSource.class).build();
+    }
+}
